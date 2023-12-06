ff6049badc1bc832f7e8fe0e91829fc93ec10853
snguyette <52304671+snguyette@users.noreply.github.com>
2023-12-05 18:50:38 -0600

    Revert "Day5"

--- code_golf/day1_golf.py
+++ code_golf/day1_golf.py
@@ -8,6 +8,7 @@ for line in get_data(day=1, year=2023).split("\n"):
     nums = [c for c in line if c.isnumeric()]
     total1 += int(f'{nums[0]}{nums[-1]}')
     nums = [m for m in re.findall(num_regex, line)]
-    for i, num in enumerate(nums): nums[i] = int(num) if num.isnumeric() else NUM_WORDS[num]
+    for i, num in enumerate(nums):
+        nums[i] = int(num) if num.isnumeric() else NUM_WORDS[num]
     total2 += int(f'{nums[0]}{nums[-1]}')
 print(f'Part 1: {total1}, Part 2: {total2}')
