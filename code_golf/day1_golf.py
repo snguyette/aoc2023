844899bccb421efc84fb0e322803ab2944eab192
snguyette <shaunathan2@gmail.com>
2023-12-09 16:01:35 -0600

    Code golf improvements for days 1, 3 and 9

--- code_golf/day1_golf.py
+++ code_golf/day1_golf.py
@@ -1,10 +1,7 @@
-from aocd import get_data; import re; total1, total2 = 0, 0
-num_regex = r'(?=(1|2|3|4|5|6|7|8|9|0|one|two|three|four|five|six|seven|eight|nine|zero))'
-NUM_WORDS = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6,
-        "seven": 7, "eight": 8, "nine": 9}
-for line in get_data(day=1, year=2023).split("\n"):
-    nums = [c for c in line if c.isnumeric()]; total1 += int(f'{nums[0]}{nums[-1]}')
-    nums = [m for m in re.findall(num_regex, line)]
-    for i, num in enumerate(nums): nums[i] = int(num) if num.isnumeric() else NUM_WORDS[num]
-    total2 += int(f'{nums[0]}{nums[-1]}')
-print(f'Part 1: {total1}, Part 2: {total2}')
+from aocd import get_data; import re; total1, t2 = 0, 0; nw = {"zero": 0, "one": 1,
+"two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+nr = r'(?=(1|2|3|4|5|6|7|8|9|0|one|two|three|four|five|six|seven|eight|nine|zero))'
+lines = get_data(day=1, year=2023).split("\n")
+t1 = sum([int(f'{n[0]}{n[-1]}') for l in lines for n in [[c for c in l if c.isnumeric()]]])
+t2 = sum([int(f'{n[0]}{n[-1]}') for l in lines for n in [[int(m) if m.isnumeric() else nw[m]
+    for m in re.findall(nr, l)]]]); print(f'Part 1: {t1}, Part 2: {t2}')
